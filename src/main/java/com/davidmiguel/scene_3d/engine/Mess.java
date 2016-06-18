db6c8d9708a7d343536893512ff10a777ab375b5
davidmigloz <me@davidmiguel.com>
2016-06-18 18:26:18 +0200

    Add mess class

---
+++ src/main/java/com/davidmiguel/scene_3d/engine/Mess.java
@@ -0,0 +1,63 @@
+package com.davidmiguel.scene_3d.engine;
+
+import javax.vecmath.Vector3d;
+
+/**
+ * Mess (3D object).
+ *
+ * @author davidmigloz
+ * @since 18/06/2016
+ */
+public class Mess {
+
+    /**
+     * Name of the 3D object.
+     */
+    private String name;
+
+    /**
+     * Collection of vertices (several vertex or 3D points).
+     */
+    private Vector3d[] vertices;
+
+    /**
+     * Its position in the 3D world.
+     */
+    private Vector3d position;
+
+    /**
+     * Its rotation state.
+     */
+    private Vector3d rotation;
+
+    public Mess(String name, int numVertices) {
+        this.name = name;
+        this.vertices = new Vector3d[numVertices];
+        this.position = new Vector3d();
+        this.rotation = new Vector3d();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Vector3d[] getVertices() {
+        return vertices;
+    }
+
+    public Vector3d getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector3d position) {
+        this.position = position;
+    }
+
+    public Vector3d getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(Vector3d rotation) {
+        this.rotation = rotation;
+    }
+}
